668e53465b7922ed39fdfc24af1f48dd8f2bf5c6
Jessica Ji <jji@berkeley.edu>
2016-01-14 20:40:45 -0800

    plot reformat

--- yt_kmeans.py
+++ yt_kmeans.py
@@ -21,7 +21,7 @@ for row in csv_f:
 X = np.array(X)
 
 # plot location points
-plt.scatter(X[:,0],X[:,1])
+plt.scatter(X[:,0],X[:,1],s=60)
 plt.show()
 
 # perform k-means algorithm on data, identify centroids
@@ -36,13 +36,13 @@ n_clusters_ = len(np.unique(labels))
 print("Number of estimated clusters: ", n_clusters_)
 
 # color each cluster and plot each with corresponding centroid
-colors = 10*['r.','g.','b.','c.','k.','y.','m.']
+colors = 10*['r.','g.','c.','k.','y.','m.']
 
 for i in range(len(X)):
-	plt.plot(X[i][0],X[i][1],colors[labels[i]],markersize =10)
+	plt.plot(X[i][0],X[i][1],colors[labels[i]],markersize =13)
 
 plt.scatter(centroids[:,0],centroids[:,1],
-	marker="x", s=150, linewidths = 5, zorder=10)
+	marker="x", s=60, linewidths = 3, zorder=10)
 plt.show()
 
 
